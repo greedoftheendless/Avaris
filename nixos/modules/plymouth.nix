7478e6016525952657057f17b822690185929d71
greedoftheendless <greedoftheendless@gmail.com>
2025-12-15 19:40:07 +0530

    added plymouth for animation

--- nixos/modules/plymouth.nix
+++ nixos/modules/plymouth.nix
@@ -3,11 +3,10 @@
 
     plymouth = {
       enable = true;
-      theme = "rings";
+      theme = "darth_vader";
       themePackages = with pkgs; [
-        # By default we would install all themes
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
+          selected_themes = [ "darth_vader" ];
         })
       ];
     };
@@ -25,7 +24,7 @@
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
-    loader.timeout = 0;
+    loader.timeout = 3;
 
   };
 }
