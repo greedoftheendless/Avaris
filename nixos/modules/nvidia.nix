8a821aa8b3d6784597ec4d851a816799f4935bd6
greedoftheendless <greedoftheendless@gmail.com>
2026-02-20 03:39:22 +0530

    put in cachyos patch for v-6.9

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.modules.nvidia;
@@ -20,6 +21,25 @@ in {
       powerManagement.enable = true;
       powerManagement.finegrained = true;
       open = true;
+
+      # Apply CachyOS kernel 6.19 patch to NVIDIA latest driver
+      package =
+        let
+          base = config.boot.kernelPackages.nvidiaPackages.latest;
+          cachyos-nvidia-patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+            sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+          };
+
+      # Patch the appropriate driver based on config.hardware.nvidia.open
+      driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+    in
+      base
+        // {
+        ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+        });
+      };
 
       prime = {
         offload = {
