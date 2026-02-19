62649965b08c0306ad51ac4f60eb8a31410fcaa7
greedoftheendless <greedoftheendless@gmail.com>
2026-02-20 03:39:41 +0530

    removed hjem

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hjem = {
-      url = "github:feel-co/hjem";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
