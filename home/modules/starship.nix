09d632a4e6cf58d54692772f5f9a06380c54a546
greedoftheendless <greedoftheendless@gmail.com>
2026-02-26 23:22:23 +0530

    updated starshi prompt file

--- home/modules/starship.nix
+++ home/modules/starship.nix
@@ -1,30 +1,28 @@
 _: {
-    programs.starship = {
-        enable = true;
-        settings = {
-            add_newline = false;
-            command_timeout = 1000;
-            line_break = {
-                disabled = true;
-            };
-            git_status = {
-              disabled = true;
-            };
-            directory = {
-                truncation_length = 3;
-                truncate_to_repo = false;
-                truncation_symbol = "…/";
-            };
-            directory.read_only = " 󰌾";
-            git_branch.symbol = " ";
-            hostname.ssh_symbol = " ";
-            meson.symbol = "󰔷 ";
-            nix_shell.symbol = " ";
-            package.symbol = "󰏗 ";
-            rust.symbol = " ";
-            python.symbol = " ";
-            lua.symbol = " ";
-        };
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      command_timeout = 1000;
+      line_break = {
+        disabled = true;
+      };
+      git_status = {
+        disabled = true;
+      };
+      directory = {
+        truncation_length = 3;
+        truncate_to_repo = false;
+        truncation_symbol = "…/";
+      };
+      directory.read_only = " 󰌾";
+      git_branch.symbol = " ";
+      hostname.ssh_symbol = " ";
+      nix_shell.symbol = " ";
+      package.symbol = "󰏗 ";
+      rust.symbol = " ";
+      python.symbol = " ";
+      lua.symbol = " ";
     };
+  };
 }
-
