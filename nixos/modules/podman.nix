7df1894447b7f28eea08c85cd385853225fad0fd
greedoftheendless <greedoftheendless@gmail.com>
2026-04-03 22:42:32 +0530

    made simple working podman config

--- nixos/modules/podman.nix
+++ nixos/modules/podman.nix
@@ -1,28 +1,11 @@
 { pkgs, ... }:
 {
-  # Enable Podman and related tools
-  virtualisation = {
-    podman = {
-      enable = true;
-    };
-    containers.enable = true;
-
-    # Create a 'docker' alias for podman
-    dockerCompat = true;
-
-    # Required for containers to communicate with each other
-    defaultNetwork.settings.dns_enabled = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true; # lets you use `docker` cli as alias
   };
 
-  # This section fixes the "short-name" error
-  virtualisation.containers.registries.search = [
-    "docker.io"
-    "quay.io"
-    "ghcr.io"
-  ];
-
-  # Useful packages for container management
   environment.systemPackages = with pkgs; [
-    podman-compose # If you want to use docker-compose syntax
+    podman-compose
   ];
 }
