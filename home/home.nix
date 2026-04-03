4545b067d1c7bcf19e486527c99970637cc8b85c
greedoftheendless <greedoftheendless@gmail.com>
2026-04-03 23:04:29 +0530

    updated

--- home/home.nix
+++ home/home.nix
@@ -11,11 +11,11 @@
     ./modules/fish.nix
     ./modules/cursor.nix
     ./modules/starship.nix
+    ./modules/zed.nix
     ./modules/nvf/nvf.nix
     ./modules/fastfetch/fastfetch.nix
     ./modules/cava/cava.nix
     ./modules/kitty/kitty.nix
-    ./modules/zed.nix
 
     #DMS shell input import
     inputs.dankMaterialShell.homeModules.dankMaterialShell.default
