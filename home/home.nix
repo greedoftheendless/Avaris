8b28c29d4e1517e570e9c6fa2fdf1bc5477c1b2c
greedoftheendless <greedoftheendless@gmail.com>
2026-04-05 00:28:57 +0530

    file clean

--- home/home.nix
+++ home/home.nix
@@ -5,25 +5,6 @@
   ...
 }:
 {
-  imports = [
-    #Module imports
-    ./home-packages.nix
-    ./modules/fish.nix
-    ./modules/cursor.nix
-    ./modules/starship.nix
-    ./modules/zed.nix
-    ./modules/nvf/nvf.nix
-    ./modules/fastfetch/fastfetch.nix
-    ./modules/cava/cava.nix
-    ./modules/kitty/kitty.nix
-
-    #DMS shell input import
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
-    inputs.dsearch.homeModules.default
-  ];
-
-  fonts.fontconfig.enable = true;
-
   home.username = "greed";
   home.homeDirectory = "/home/greed";
   home.stateVersion = "24.05";
