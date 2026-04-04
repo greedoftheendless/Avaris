cc51ceab9b72dd934f9c0f96d3ff7eb55b7f0c44
greedoftheendless <greedoftheendless@gmail.com>
2026-04-05 00:30:24 +0530

    file to point flake for nh. and configure nh globally

---
+++ nixos/modules/flake-point.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  #Adding nix-experimental command features and nix flakes
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  #Pointing nh the direction of flake
+  environment.sessionVariables = {
+    NH_FLAKE = "/home/greed/dotfiles";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  nix.package = pkgs.nixVersions.latest;
+}
