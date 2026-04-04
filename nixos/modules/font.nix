822ac891559c7ad7ac1359d5e0486e5b49d3fdd3
greedoftheendless <greedoftheendless@gmail.com>
2026-04-05 00:30:46 +0530

    file for font. declare formally

---
+++ nixos/modules/font.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: # Remove comma here
+{
+  #Fonts
+  fonts = {
+    fontconfig.enable = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.fira-mono
+      nerd-fonts._0xproto
+      nerd-fonts.iosevka-term-slab
+      fira-code-symbols
+      noto-fonts
+      noto-fonts-lgc-plus
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      papirus-icon-theme
+      tokyonight-gtk-theme
+    ];
+  };
+}
