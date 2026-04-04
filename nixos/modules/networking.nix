d099d3678db7451f645eddfb69f130f9ba5ffa50
greedoftheendless <greedoftheendless@gmail.com>
2026-04-05 00:30:55 +0530

    file for configuring network

---
+++ nixos/modules/networking.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable SSH
+  services.openssh.enable = true;
+
+  #Adding bluetooth
+  services.blueman.enable = true;
+
+  #Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  networking.hostName = "greed";
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+}
