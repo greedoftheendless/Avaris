555850a83a461cf7354bfca8d6391f11469cace9
greedoftheendless <greedoftheendless@gmail.com>
2026-05-05 03:16:50 +0530

    added nixcord and removed nix fmt

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    nixcord.url = "github:FlameFlag/nixcord";
+
     quickshell = {
       url = "github:outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,6 +17,11 @@
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    mangowm = {
+      url = "github:mangowm/mango";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -46,79 +52,32 @@
 
     systems.url = "github:nix-systems/default";
   };
-  outputs =
-    inputs@{
-      self,
-      systems,
-      ...
-    }:
-    let
-      forEachSystem = inputs.nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      # Run hooks with `nix fmt`
-      formatter = forEachSystem (
-        system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-          config = self.checks.${system}.pre-commit-check.config;
-          inherit (config) package configFile;
-          script = ''
-            ${pkgs.lib.getExe package} run --all-files --config ${configFile}
-          '';
-        in
-        pkgs.writeShellScriptBin "pre-commit-run" script
-      );
-      # Run hooks in sandbox with `nix flake check`
-      checks = forEachSystem (system: {
-        pre-commit-check = inputs.git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            nixfmt.enable = true; # formats .nix files
-            nil.enable = true; # nix semantic error checking
-            typos.enable = true; # spell check code and comments
-            check-merge-conflicts.enable = true; # catch unresolved merge conflicts
-            check-case-conflicts.enable = true; # catch case-insensitive filename clashes
-            check-added-large-files = {
-              # change this
-              enable = true;
-              args = [ "--maxkb=15000" ];
+  outputs = inputs @ {
+    self,
+    systems,
+    mangowm,
+    ...
+  }: let
+  in {
+    nixosConfigurations = {
+      greed = inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./nixos/default.nix
+          ./nixos/system-packages.nix
+          mangowm.nixosModules.mango
+          inputs.home-manager.nixosModules.home-manager
+          {
+            nix.settings.auto-optimise-store = true;
+            # automatic garbage collection of nix generations
+            nix.gc = {
+              automatic = true;
+              dates = "daily";
+              options = "--delete-older-than 10d";
             };
-          };
-        };
-      });
-      # Enter dev shell with `nix develop` — hooks install automatically
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-          inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
-        in
-        {
-          default = pkgs.mkShell {
-            inherit shellHook;
-            buildInputs = enabledPackages;
-          };
-        }
-      );
-      nixosConfigurations = {
-        greed = inputs.nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./nixos/default.nix
-            ./nixos/system-packages.nix
-            inputs.home-manager.nixosModules.home-manager
-            {
-              nix.settings.auto-optimise-store = true;
-              # automatic garbage collection of nix generations
-              nix.gc = {
-                automatic = true;
-                dates = "daily";
-                options = "--delete-older-than 10d";
-              };
-            }
-          ];
-        };
+          }
+        ];
       };
     };
+  };
 }
