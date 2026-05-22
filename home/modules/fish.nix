425963e2beb68a645e3ae7b0c3135a2b6e2e7de8
greedoftheendless <greedoftheendless@gmail.com>
2026-05-22 10:43:53 +0530

    removed wiki-tui alias

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -8,7 +8,6 @@
       grep = "rg";
       lgit = "lazygit";
       zed = "zeditor";
-      wiki = "wiki-tui";
       #for nh/nix update and switch
       update = "nh os switch -u";
       rebuild = "nh os switch";
@@ -37,33 +36,33 @@
     ];
 
     interactiveShellInit = ''
-      # Greeting
-      set -g fish_greeting "🦊 Welcome, $USER!"
+       # Greeting
+       set -g fish_greeting "🦊 Welcome, $USER!"
 
       # Atuin shell history
-      atuin init fish | sed 's/-k up/up/' | source
+       atuin init fish | sed 's/-k up/up/' | source
 
-      # Zoxide integration
-      zoxide init fish | source
+       # Zoxide integration
+       zoxide init fish | source
 
-      # Navi widget
-      navi widget fish | source
+       # Navi widget
+       navi widget fish | source
 
-      # Pipx user bin path
-      set -gx PATH $PATH $HOME/.local/bin
+       # Pipx user bin path
+       set -gx PATH $PATH $HOME/.local/bin
 
-      # Enable completion colors
-      set -g fish_color_autosuggestion brblack
-      set -g fish_color_command green
-      set -g fish_color_param white
-      set -g fish_color_comment yellow
+       # Enable completion colors
+       set -g fish_color_autosuggestion brblack
+       set -g fish_color_command green
+       set -g fish_color_param white
+       set -g fish_color_comment yellow
 
-      # Universal variables (example subset)
-      set -U fish_color_cancel -r
-      set -U fish_color_command normal
-      set -U fish_color_comment red
-      set -U fish_color_user brgreen
-      set -U fish_user_paths /nix/store/i24mfkgsg2zjqh0jasw2pw3jxjqldp-ghostty-1.2.2/bin
+       # Universal variables (example subset)
+       set -U fish_color_cancel -r
+       set -U fish_color_command normal
+       set -U fish_color_comment red
+       set -U fish_color_user brgreen
+       set -U fish_user_paths /nix/store/i24mfkgsg2zjqh0jasw2pw3jxjqldp-ghostty-1.2.2/bin
     '';
   };
 }
