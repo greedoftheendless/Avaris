b2466196c85035905e0db0c8c3ab95bedb52b869
greedoftheendless <greedoftheendless@gmail.com>
2026-06-04 09:04:35 +0300

    deleted pics

---
+++ nixos/modules/system-packages.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  #Installing Packages
+  programs.steam.enable = true;
+
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+  };
+
+  environment.systemPackages = with pkgs; [
+    #Terminal tools
+    xwayland
+    xwayland-satellite
+    xwayland-run
+    xdg-desktop-portal-gnome
+    nh
+    libsForQt5.qt5.qttools
+    upower-notify
+    pkgs.adwaita-icon-theme
+    swaynotificationcenter
+  ];
+}
