bca25132aef217d0c43cb33762eb4b5456f40502
greedoftheendless <greedoftheendless@gmail.com>
2026-08-20 19:06:21 +0530

    flake has zennotes url removed since package has arrived

--- flake.nix
+++ flake.nix
@@ -11,18 +11,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    herdr = {
-      url = "github:herdrdev/herdr";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    zen-notes = {
-      url = "github:ZenNotes/zennotes";
     };
 
     noctalia = {
