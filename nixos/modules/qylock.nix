3559bdd219aa8d174a2967e2a7d971f9122c59c0
greedoftheendless <greedoftheendless@gmail.com>
2026-08-21 23:38:32 +0300

    added qylock.nix file and also added list of working themes

--- nixos/modules/qylock.nix
+++ nixos/modules/qylock.nix
@@ -1,12 +1,14 @@
-{
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   imports = [inputs.qylock.nixosModules.default];
 
   programs.qylock = {
     enable = true;
-    theme = "osu";
+
+    # theme = "osu";
+    # theme = "ninja_gaiden";
+    theme = "clockwork/orbit";
+    # theme = "nier-automata";
+
     sddm.enable = true;
     quickshell.enable = true;
 
