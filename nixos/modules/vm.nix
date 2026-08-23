69d06e13dbfc02cfe4d0e25220488c66a71bbe2d
greedoftheendless <greedoftheendless@gmail.com>
2026-08-23 10:53:38 +0300

    added vm.nix file for vm's. Thanks to iynaix

--- nixos/modules/vm.nix
+++ nixos/modules/vm.nix
@@ -1,26 +1,21 @@
-{
-  flake.modules.nixos.services_virtualisation = {
-    config,
-    pkgs,
-    ...
-  }: let
-    inherit (config.custom.constants) user;
-  in {
-    config = {
-      virtualisation = {
-        libvirtd.enable = true;
-        # following configuration is used only when building VMs with build-vm
-        vmVariant = {
-          virtualisation = {
-            memorySize = 1024 * 6;
-            cores = 4;
-          };
-        };
-      };
-      programs.virt-manager.enable = true;
-      environment.systemPackages = with pkgs; [virtiofsd];
+{pkgs, ...}: let
+  user = "greed";
+in {
+  virtualisation = {
+    libvirtd.enable = true;
 
-      users.users.${user}.extraGroups = ["libvirtd"];
+    # following configuration is used only when building VMs with build-vm
+    vmVariant = {
+      virtualisation = {
+        memorySize = 1024 * 6;
+        cores = 4;
+      };
     };
   };
+
+  programs.virt-manager.enable = true;
+
+  environment.systemPackages = with pkgs; [virtiofsd];
+
+  users.users.${user}.extraGroups = ["libvirtd"];
 }
