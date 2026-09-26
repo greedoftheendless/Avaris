5e10f3fd744f39c88afc38512f01f59de2f7cd9d
greedoftheendless <greedoftheendless@gmail.com>
2026-09-26 10:44:52 +0300

    removed some parts unecessary for fish

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   programs.fish = {
     enable = true;
     shellAliases = {
@@ -10,6 +10,7 @@
       nv = "nvim";
       yz = "yazi";
       lz = "lazygit";
+
       #Everyday commands
       cat = "bat";
       cd = "z";
@@ -17,6 +18,7 @@
       la = "eza -a --long --icons --color=always";
       "..." = "cd ../..";
       ".." = "cd ..";
+
       #for git commands
       lgit = "lazygit";
       gd = "git diff";
@@ -24,6 +26,7 @@
       gs = "git status";
       gc = "git commit -m";
       gp = "git push";
+
       #Alias for jj
       jji = "jj split -i";
       jjb = "jj bookmark list";
@@ -36,32 +39,29 @@
       jjp = "jj git push --bookmark";
     };
 
-    plugins = with pkgs.fishPlugins; [
-      {
-        name = "z";
-        src = z.src;
-      }
-      {
-        name = "fzf";
-        src = fzf.src;
-      }
-    ];
+    # plugins = with pkgs.fishPlugins; [
+    #   {
+    #     name = "z";
+    #     src = z.src;
+    #   }
+    #   {
+    #     name = "fzf";
+    #     src = fzf.src;
+    #   }
+    # ];
 
     interactiveShellInit = ''
-       # Greeting
-       set -g fish_greeting "🦊 Welcome, $USER!"
+      # Greeting
+      set -g fish_greeting "🦊 Welcome, $USER!"
 
-       # Disable fish autosuggestions (atuin handles them)
-       # set -g fish_autosuggestion_enabled 0
+      # Enable/Disable fish autosuggestions
+      # set -g fish_autosuggestion_enabled 0
 
       # Atuin shell history
        atuin init fish | sed 's/-k up/up/' | source
 
        # Zoxide integration
        zoxide init fish | source
-
-       # Pipx user bin path
-       set -gx PATH $PATH $HOME/.local/bin
 
        # Enable completion colors
        set -g fish_color_autosuggestion brblack
@@ -76,14 +76,10 @@
        set -U fish_color_user brgreen
        set -U fish_user_paths /nix/store/i24mfkgsg2zjqh0jasw2pw3jxjqldp-ghostty-1.2.2/bin
 
-      #completions
+       # Completions
        set -p fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d
        set -p fish_complete_path /etc/profiles/per-user/$USER/share/fish/vendor_completions.d
        set -p fish_complete_path /run/current-system/sw/share/fish/vendor_completions.d
-
-       # Navi cheat sheet widget
-       bind \cg _navi_smart_replace
-       bind --mode insert \cg _navi_smart_replace
     '';
   };
 }
